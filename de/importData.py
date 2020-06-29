c5a9ebf3e01e532cec550e8b1b8c47dbb701d608
Madison Wahlsten <mlwahl@umich.edu>
2020-06-29 12:41:10 -0700

    fixed normalization of PCA, separated functions from script for PCA, added calls to compare PCA with replicates averaged or not

--- de/importData.py
+++ de/importData.py
@@ -6,4 +6,7 @@ def importRNAseqKO():
     data = pd.read_csv("data/rpmCounts_allRuns_matrix.tsv.xz", index_col="GeneSymbol", delim_whitespace=True)
     data = data.drop(["gene_id"], axis=1) # using GeneSymbol as index
     data = data.dropna(axis=1) # remove columns with no measurements
+    # remove excess info from knockout names to make replicate names identical
+    KO_genes = list(list(zip(*data.columns.str.split("-")))[0])
+    data.columns = KO_genes
     return data
