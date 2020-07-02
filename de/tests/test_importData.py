835960ffd4af8174e4a83864f98ef315cfa22224
Madison Wahlsten <mlwahl@umich.edu>
2020-07-02 13:04:13 -0700

    updated test file to account for diagonal of matrix being nonzero

--- de/tests/test_importData.py
+++ de/tests/test_importData.py
@@ -16,8 +16,9 @@ class TestModel(unittest.TestCase):
         self.assertTrue(isinstance(data, pd.DataFrame))
 
     def test_matrix(self):
-        """Tests that matrix formed is reasonable"""
+        """Tests that matrix formed is an 83x84 DataFrame with matching knockouts/gene measurements on the diagonal"""
         matrix = formMatrix(importRNAseqKO())
         self.assertTrue(isinstance(matrix, pd.DataFrame))
-        for i in enumerate(matrix.rows):
-            self.assertTrue(matrix.iloc[i][i]==0)
+        self.assertEqual(matrix.shape[0], 83) # there were 83 knockout models and thus associated genes
+        self.assertEqual(matrix.shape[1], 84) # there is an extra column due to the negative control
+        self.assertEqual(matrix.loc["NFAT5", "NFAT5"], matrix.iloc[42, 42]) # check diagonal is matched pairs
