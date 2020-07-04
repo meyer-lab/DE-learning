bfa9f922ca6543bd4c8c52fcc2d4aa81b3b83578
Zhanzhang-2020 <11712523@mail.sustech.edu.cn>
2020-07-04 21:59:41 +0800

    Results of basic ODE simulation

---
+++ de/ODE.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.integrate import odeint
+
+def solver(n_x,x0,eps,w,alpha,beta,dt,N_t):
+    '''Function receives time series and a set of parameters,
+       then return the simulation of ODE.
+    '''
+    t = np.linspace(0,dt*N_t,N_t)
+    sol = odeint(ODE,x0,t,args=(eps,w,alpha,beta))
+    return t,sol
+
+    
+def ODE(y,t,eps,w,alpha,beta):
+    '''The system of ODE'''
+    envelop_fn = np.tanh
+    x = y
+    dydt = eps * envelop_fn(np.dot(w,x)) - (alpha*beta) * x 
+    #eps: Value that bound the saturation effect
+    #w: Interaction between components
+    #alpha: Degradation rate
+    #beta: Knock-out effects
+    return dydt
+
---
+++ de/model.py
@@ -0,0 +1,47 @@
+'''
+Basic Model simulation
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from ODE import solver,ODE
+
+class Model(object):
+    def __init__(self, n_x=83, pert=50, index=np.loadtxt('./params/node_Index.csv', delimiter=',', dtype=str), dt=0.1, N_t=100, filename='./figures/simulation_1.jpg'):
+        self.n_x = n_x #The number of components involved
+        self.x0 = np.ones(n_x) #Initial values
+        self.pert = pert # The strength of pertubation
+        self.index = index # The gene name for each component
+        self.dt, self.N_t = dt, N_t # Time series
+        self.filename = filename #Path for saving simulation result
+
+    def graph(self):
+        '''Function receives simulation of ODE and the number of components involved, then generate the graph'''
+        fig = plt.figure(figsize=(100,300.0))
+        for i in range(self.n_x):
+            axes = fig.add_subplot(math.ceil(self.n_x/5),5,i+1)
+            axes.plot(self.t,self.sol[:,i],'r')
+            axes.set_title(self.index[i])
+        plt.xlabel('t')
+        plt.ylabel('relative concentration')   # x(t)/x(0)
+        fig.tight_layout()
+        plt.savefig(self.filename)
+        plt.show()
+        
+    def sim(self):
+        '''Run the ODE model'''
+        self.t, self.sol = solver(self.n_x,self.x0,self.eps,self.w,self.alpha,self.beta,self.dt,self.N_t)
+        
+    def random_params(self):
+        '''Randomly initialize the parameters based on the number of components and pertubation strength.
+           only consider one knock-out condition here.
+        '''
+        self.eps = np.random.normal(1,1.0,size=(self.n_x))
+        W = np.random.normal(0.01,1.0,size=(self.n_x,self.n_x))
+        W_mask = (1.0 - np.diag(np.ones([self.n_x]))) #remove self-interaction
+        self.w = W_mask*W
+        self.alpha = np.random.normal(2,1.0,size=(self.n_x))
+        self.beta = np.ones(self.n_x)
+        self.beta[0] = self.pert
+
