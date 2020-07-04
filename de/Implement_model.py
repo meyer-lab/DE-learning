31cb636438307f5a00534ba34be730f56ec98959
Zhanzhang-2020 <11712523@mail.sustech.edu.cn>
2020-07-04 21:59:58 +0800

    Revert "Results of basic ODE simulation"
    This reverts commit bfa9f922ca6543bd4c8c52fcc2d4aa81b3b83578.

--- de/Implement_model.py
+++
@@ -1,8 +0,0 @@
-'''implement the basic model simulation'''
-from model import Model
-
-test1 = Model()
-test1.random_params() # Randomly initialize parameters
-test1.sim() # Run ODE model
-test1.graph() # Results of simulation
-
