50e07836b8aae44e5949381694ad34d8d40f410b
Zhanzhang-2020 <11712523@mail.sustech.edu.cn>
2020-07-06 15:28:54 +0800

    Update Test_Simulation.py

--- de/tests/Test_Simulation.py
+++ de/tests/Test_Simulation.py
@@ -1,9 +1,9 @@
-'''Add a test to the simulation of ODE. 
-   The dimension of parameters(eps,w,alpha,beta) should be n_x, n_x*n_x, n_x, n_x.
-   Expected results would be an array with N_t columns and nx rows.
-   In addition, the relative concentration of knockout component should approach zero.
 '''
-
+Add a test to the simulation of ODE.
+The dimension of parameters(eps,w,alpha,beta) should be n_x, n_x*n_x, n_x, n_x.
+Expected results would be an array with N_t columns and nx rows.
+In addition, the relative concentration of knockout component should approach zero.
+'''
 import unittest
 import numpy as np
 from model import Model
@@ -11,18 +11,23 @@ from model import Model
 class Test_ODE(unittest.TestCase):
     '''Test class for basic simulation of ODE systems'''
     def test_params(self):
+        '''
+        Test the dimension of parameters
+        '''
         test1 = Model()
         test1.random_params()
-        self.assertEqual(np.shape(test1.eps), (test1.n_x,))
-        self.assertEqual(np.shape(test1.w), (test1.n_x,test1.n_x))
-        self.assertEqual(np.shape(test1.alpha), (test1.n_x,))
-        self.assertEqual(np.shape(test1.beta), (test1.n_x,))
-        
+        self.assertEqual(np.shape(test1.eps), (test1.n_x, ))
+        self.assertEqual(np.shape(test1.w), (test1.n_x, test1.n_x))
+        self.assertEqual(np.shape(test1.alpha), (test1.n_x, ))
+        self.assertEqual(np.shape(test1.beta), (test1.n_x, ))
     def test_simulation(self):
+        '''
+        Test the simulation of ODE model
+        '''
         test1 = Model()
         test1.random_params()
         test1.sim()
         self.assertTrue(isinstance(test1.sol, np.ndarray))
-        self.assertEqual(len(test1.sol[:,0]), test1.N_t)
-        self.assertEqual(len(test1.sol[0,:]), test1.n_x)
-        self.assertEqual(int(test1.sol[50,0]), 0)
+        self.assertEqual(len(test1.sol[:, 0]), test1.N_t)
+        self.assertEqual(len(test1.sol[0, :]), test1.n_x)
+        self.assertEqual(int(test1.sol[50, 0]), 0)
