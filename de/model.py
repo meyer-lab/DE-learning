2f0ec041e1d2c64488f832c81f1ba9dac4e4e0fd
Zhanzhang-2020 <11712523@mail.sustech.edu.cn>
2020-07-06 15:05:39 +0800

    Update model.py

--- de/model.py
+++ de/model.py
@@ -15,15 +15,15 @@ class Model(object):
         self.index = index # The gene name for each component
         self.dt, self.N_t = dt, N_t # Time series
         self.filename = filename #Path for saving simulation result
-        self.t, self.sol= self.sim(self) #Time series and simulation of ODE
+        self.t, self.sol = self.sim(self) #Time series and simulation of ODE
         self.eps, self.w, self.alpha, self.beta = self.random_params(self) #Randomly initialized parametersi
 
     def graph(self):
         '''Function receives simulation of ODE and the number of components involved, then generate the graph'''
-        fig = plt.figure(figsize=(100,300.0))
+        fig = plt.figure(figsize=(100, 300.0))
         for i in range(self.n_x):
-            axes = fig.add_subplot(math.ceil(self.n_x/5),5,i+1)
-            axes.plot(self.t,self.sol[:,i],'r')
+            axes = fig.add_subplot(math.ceil(self.n_x/5), 5, i+1)
+            axes.plot(self.t, self.sol[:,i], 'r')
             axes.set_title(self.index[i])
         plt.xlabel('t')
         plt.ylabel('relative concentration')   # x(t)/x(0)
@@ -33,18 +33,18 @@ class Model(object):
         
     def sim(self):
         '''Run the ODE model'''
-        t, sol= solver(self.x0,self.eps,self.w,self.alpha,self.beta,self.dt,self.N_t)
+        t, sol= solver(self.x0, self.eps, self.w, self.alpha, self.beta, self.dt, self.N_t)
         return t, sol
         
     def random_params(self):
         '''Randomly initialize the parameters based on the number of components and pertubation strength.
            only consider one knock-out condition here.
         '''
-        eps = np.random.normal(1,1.0,size=(self.n_x))
-        W = np.random.normal(0.01,1.0,size=(self.n_x,self.n_x))
+        eps = np.random.normal(1, 1.0, size=(self.n_x))
+        W = np.random.normal(0.01, 1.0, size=(self.n_x, self.n_x))
         W_mask = (1.0 - np.diag(np.ones([self.n_x]))) #remove self-interaction
         w = W_mask*W
-        alpha = np.random.normal(2,1.0,size=(self.n_x))
+        alpha = np.random.normal(2, 1.0, size=(self.n_x))
         beta = np.ones(self.n_x)
         beta[0] = self.pert
         return eps, w, alpha, beta
