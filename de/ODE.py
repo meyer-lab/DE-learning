e872f83809d245b91b5164ec9846c261ba5a6e0d
Zhanzhang-2020 <11712523@mail.sustech.edu.cn>
2020-07-06 08:30:41 +0800

    Update ODE.py

--- de/ODE.py
+++ de/ODE.py
@@ -1,23 +1,22 @@
 import numpy as np
 from scipy.integrate import odeint
 
-def solver(x0,eps,w,alpha,beta,dt,N_t):
+def solver(x0, eps, w, alpha, beta, dt, N_t):
     '''Function receives time series and a set of parameters,
        then return the simulation of ODE.
     '''
-    t = np.linspace(0,dt*N_t,N_t)
-    sol = odeint(ODE,x0,t,args=(eps,w,alpha,beta))
-    return t,sol
+    t = np.linspace(0, dt*N_t, N_t)
+    sol = odeint(ODE, x0, t, args=(eps, w, alpha, beta))
+    return t, sol
 
     
-def ODE(y,t,eps,w,alpha,beta):
+def ODE(y, t, eps, w, alpha, beta):
     '''The system of ODE'''
     envelop_fn = np.tanh
     x = y
-    dydt = eps * envelop_fn(np.dot(w,x)) - (alpha*beta) * x 
+    dydt = eps * envelop_fn(np.dot(w, x)) - (alpha*beta) * x 
     #eps: Value that bound the saturation effect
     #w: Interaction between components
     #alpha: Degradation rate
     #beta: Knock-out effects
     return dydt
-
