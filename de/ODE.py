d57067e105145177e706c55d3510a0b6c39dbc8e
Zhanzhang-2020 <11712523@mail.sustech.edu.cn>
2020-07-06 08:39:55 +0800

    Update ODE.py

--- de/ODE.py
+++ de/ODE.py
@@ -1,3 +1,6 @@
+'''
+This file contains ODE equations and solver
+'''
 import numpy as np
 from scipy.integrate import odeint
 
@@ -8,15 +11,15 @@ def solver(x0, eps, w, alpha, beta, dt, N_t):
     t = np.linspace(0, dt*N_t, N_t)
     sol = odeint(ODE, x0, t, args=(eps, w, alpha, beta))
     return t, sol
-
-    
 def ODE(y, t, eps, w, alpha, beta):
     '''The system of ODE'''
     envelop_fn = np.tanh
     x = y
     dydt = eps * envelop_fn(np.dot(w, x)) - (alpha*beta) * x 
-    #eps: Value that bound the saturation effect
-    #w: Interaction between components
-    #alpha: Degradation rate
-    #beta: Knock-out effects
+    '''
+    eps: Value that bound the saturation effect
+    w: Interaction between components
+    alpha: Degradation rate
+    beta: Knock-out effects
+    '''
     return dydt
