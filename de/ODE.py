eb119a5333f20b33ee48f2cbc9521b0ea23b7d93
Zhanzhang-2020 <11712523@mail.sustech.edu.cn>
2020-07-06 08:19:29 +0800

    Update ODE.py

--- de/ODE.py
+++ de/ODE.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.integrate import odeint
 
-def solver(n_x,x0,eps,w,alpha,beta,dt,N_t):
+def solver(x0,eps,w,alpha,beta,dt,N_t):
     '''Function receives time series and a set of parameters,
        then return the simulation of ODE.
     '''
