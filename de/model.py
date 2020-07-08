5a1c14f9e01a6d207cb7de4d2f8436850da36113
Zhanzhang-2020 <11712523@mail.sustech.edu.cn>
2020-07-08 21:28:34 +0800

    Add function for calculating Jacobian Matrix of the system

--- de/model.py
+++ de/model.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
-from ODE import solver
+from ODE import solver, jacobian_autograd
 
 class Model():
     '''
@@ -28,6 +28,7 @@ class Model():
         self.alpha = None #Degradation rate
         self.beta = None #Knock-out effects
         self.sse = None # The sum of square error across all measurements
+        self.jacb = None # The jacobian matrix of the system
 
     def graph(self, cond):
         '''Function receives simulation of ODE and the number of components involved, then generate the graph.
@@ -46,6 +47,9 @@ class Model():
     def sim(self):
         '''Run the ODE model'''
         self.t, self.sol = solver(self.n_x, self.N, self.x0, self.eps, self.w, self.alpha, self.beta, self.dt, self.N_t)
+    def jac(self):
+        ''' Obtain the jacobian matrix of the system'''
+        self.jacb = jacobian_autograd(self.sol, self.eps, self.w, self.alpha, self.beta, self.N, self.n_x)
     def random_params(self):
         '''
         Randomly initialize the parameters based on the number of components and pertubation strength.
