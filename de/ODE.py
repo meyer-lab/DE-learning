fix params of jacobian and add some lines

de/ODE.py
@@ -20,6 +20,7 @@ def solver(n_x, N, x0, p, beta, dt, N_t):
     for i in range(N):
         sol[i, :, :] = odeint(ODE, x0, t, args=(eps, w, alpha, beta[:, i]))
     return t, sol
+
 def ODE(y, t, eps, w, alpha, beta):
     '''The ODE system:
     Parameters = eps: Value that bound the saturation effect
@@ -28,14 +29,19 @@ def ODE(y, t, eps, w, alpha, beta):
                  beta: Knock-out effects
     '''
     return eps * (1 + np.tanh(np.dot(w, y))) - (alpha*beta) * y
-def jacobian_autograd(y, eps, w, alpha, beta, N, n_x):
+
+def jacobian_autograd(y, p, beta, N, n_x):
     '''
     Given a set of parameters and the state of system, it will return the Jacobian of the system.
     '''
+    eps = p[0:n_x]
+    w = p[n_x:(n_x ** 2 + n_x)].reshape((n_x,n_x))
+    alpha = p[(n_x ** 2 + n_x):]
     jac = np.zeros((N, n_x, n_x))
     for i in range(N):
         jac[i, :, :] = jacobian(ODE_anp)(y[i, -1, :], eps, w, alpha, beta[:, i])
     return jac
+
 def ODE_anp(y, eps, w, alpha, beta):
     '''
     Autograd-packed ODE function.

de/model.py
@@ -18,7 +18,7 @@ class Model():
         self.x0 = np.ones(n_x) #Initial values
         self.pert = pert # The strength of pertubation
         #TODO: Fix paths for loading data
-        #self.index = np.loadtxt(index, delimiter=',', dtype=str) # The gene name for each component
+        self.index = np.loadtxt(index, delimiter=',', dtype=str) # The gene name for each component
         self.dt, self.N_t = dt, N_t # Time step and total length of time for generating time series
         self.save_path = save_path #Path for saving simulation result
         self.expdata_path = expdata_path #Path for inputing experiment data
@@ -39,7 +39,7 @@ class Model():
             axes.plot(self.t, self.sol[cond, :, i], 'r')
             axes.set_title(self.index[i])
         plt.xlabel('t')
-        plt.ylabel('relative concentration')   # x(t)/x(0)
+        plt.ylabel('Expression Level')
         fig.tight_layout()
         plt.savefig(self.save_path)
         plt.show()
@@ -51,7 +51,7 @@ class Model():
 
     def jac(self):
         ''' Obtain the jacobian matrix of the system'''
-        self.jacb = jacobian_autograd(self.sol, self.eps, self.w, self.alpha, self.beta, self.N, self.n_x)
+        self.jacb = jacobian_autograd(self.sol, self.p, self.beta, self.N, self.n_x)
 
     def random_params(self):
         '''