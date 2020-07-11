fix Jacobian Matrix

de/ODE.py
@@ -7,7 +7,7 @@ import autograd.numpy as anp
 from autograd import jacobian
 from numba import njit
 
-def solver(n_x, N, x0, p, beta, N_t):
+def solver(n_x, N, x0, N_t, p, beta):
     '''
     Function receives time series and a set of parameters,
        then return the simulation of ODE.
@@ -16,11 +16,11 @@ def solver(n_x, N, x0, p, beta, N_t):
     eps = p[0:n_x]
     w = p[n_x:(n_x ** 2 + n_x)].reshape((n_x, n_x))
     alpha = p[(n_x ** 2 + n_x):]
-    sol = np.ones((N, N_t, n_x))
+    sol = np.ones((N, n_x))
     t = np.arange(N_t)
     for i in range(N):
-        sol[i, :, :] = np.transpose(solve_ivp(ODE, (0, N_t), x0, args=(eps, w, alpha, beta[:, i]), t_eval=t, method="LSODA").y)
-    return t, sol
+        sol[i, :] = np.transpose(solve_ivp(ODE, (0, N_t), x0, args=(eps, w, alpha, beta[:, i]), t_eval=[N_t], jac = jacobian_autograd, method="LSODA").y)
+    return sol
 
 @njit
 def ODE(t, y, eps, w, alpha, beta):
@@ -32,7 +32,7 @@ def ODE(t, y, eps, w, alpha, beta):
     '''
     return eps * (1 + np.tanh(np.dot(w, y))) - (alpha*beta) * y
 
-def jacobian_autograd(y, eps, w, alpha, beta):
+def jacobian_autograd(t, y, eps, w, alpha, beta):
     '''
     Given a set of parameters and the state of system, it will return the Jacobian of the system.
     '''

de/model.py
@@ -12,10 +12,11 @@ class Model():
     '''
     The class for basic model simulation
     '''
-    def __init__(self, n_x=83, N=84):
+    def __init__(self, n_x=83, N=84, N_t = 48):
         self.n_x = n_x #The number of components/genes involved
         self.N = N #The number of knockout conditions
         self.x0 = np.ones(n_x) #Initial values
+        self.N_t = N_t #Total length of simulation time
 
     def graph(self, cond, t, sol, save_path):
         '''Function receives simulation of ODE, the number of components involved, time series, model solution, and path at which to save image. 
@@ -33,22 +34,12 @@ class Model():
         plt.savefig(save_path)
         plt.show()
 
-    def sim(self, params, beta_in, N_t):
+    def sim(self, params, beta_in):
         '''Function receives simulation of ODE, 1D array of optimizable parameters, array of beta parameter, and time series.
         Runs the ODE model and returns solution at last timepoint.'''
-        t, sol = solver(self.n_x, self.N, self.x0, params, beta_in, N_t)
-        return np.transpose(sol[:, -1, :])
-
-    def jac(self, p, beta, sol):
-        '''Function receives simulation of ODE, 1D array of optimizable parameters, array of beta parameter, and model solution. 
-        Returns the jacobian matrix of the system'''
-        # TODO: Does jacobian require solution over time?
-        eps = p[0:self.n_x]
-        w = p[self.n_x:(self.n_x ** 2 + self.n_x)].reshape((self.n_x, self.n_x))
-        alpha = p[(self.n_x ** 2 + self.n_x):]
-        jacb = jacobian_autograd(sol, eps, w, alpha, beta)
-        return jacb
-
+        sol = solver(self.n_x, self.N, self.x0, self.N_t, params, beta_in)
+        return np.transpose(sol)
+    
     def random_params(self, pert):
         '''
         Function receives simulation of ODE and perturbation strength and randomly initialize the parameters based on the number of components.

de/optimize.py
@@ -4,12 +4,12 @@ File contains function for finding residual between model data and experimental 
 from scipy.optimize import least_squares
 from .model import Model
 
-def residual_fun(params, model, exp_data):
+def residual_fun(params, beta_in, model, exp_data):
     """Function takes in optimizable parameters, Model() instance, and experimental data matrix; returns 1D array of residuals."""
-    model_data = model.sim(params)
+    model_data = model.sim(params, beta_in)
     return model_data.flatten()-exp_data.flatten()
 
-def lsq(params, model, exp_data, fun):
+def lsq(params, model, beta_in, exp_data, fun):
     """
     Parameters:
     - params: 1D array of eps, w, and alpha values to be optimized
@@ -18,5 +18,5 @@ def lsq(params, model, exp_data, fun):
     - fun: function that returns 1D array of residuals
     Returns 1D array of optimized parameters and minimized value of Cost function using scipy.optimize.least_squares
     """
-    opt_params, cost = least_squares(residual_fun, params, args=(model, exp_data))
+    opt_params, cost = least_squares(residual_fun, params, args=(beta_in, model, exp_data))
     return opt_params, cost

de/tests/test_simulation.py
@@ -25,6 +25,6 @@ class Test_ODE(unittest.TestCase):
         '''
         test1 = Model()
         p, beta = test1.random_params(1000)
-        sol = test1.sim(p, beta, 48)
+        sol = test1.sim(p, beta)
         self.assertTrue(isinstance(sol, np.ndarray))
         self.assertEqual(sol.shape, (test1.n_x, test1.N))