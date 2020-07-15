3699fd4f39d39cf6f8152a7e265fe54ea9d048df
Madison Wahlsten <33466074+mlwahl@users.noreply.github.com>
2020-07-15 12:46:36 -0400

    Julia implementation (#28)
    * Proof of concept setup.
    
    * Some partial fixes.
    
    * updated model.jl functions and began working on python integration
    
    * modified test_simulation
    
    * fixed test
    
    * Delete optimize.py
    
    * fixed if-else statement error
    
    * added sum of w values to cost function
    
    * changed experimental data to parameter of cost function and changed optimize call accordingly
    
    * fixed gradient errors
    
    * Some minor edits.
    
    * Add timepoint parameter.
    
    * removed python model class and ODE solvers
    
    * fixed py_model tests
    
    * fixed pylint errors
    
    Co-authored-by: Aaron Meyer <git@ameyer.me>

---
+++ de/model.jl
@@ -0,0 +1,114 @@
+using OrdinaryDiffEq
+using LinearAlgebra
+using DiffEqSensitivity
+using Optim
+using Zygote
+using DelimitedFiles
+
+" Load the experimental data matrix. "
+function get_data(path_RNAseq)
+    # Import RNAseq data as 83 x 84 matrix preprocessed using python
+    exp = DelimitedFiles.readdlm(path_RNAseq, ',', Float64)
+    exp = Matrix(exp)
+    return exp
+end
+
+" Reshape a vector of parameters into the variables we know. "
+@views function reshapeParams(p)
+    w = reshape(p[1:6889], (83, 83))
+    alpha = p[6890:6972]
+    eps = p[6973:7055]
+    
+    @assert length(eps) == 83
+    @assert length(alpha) == 83
+    
+    return w, alpha, eps
+end
+
+" Melt the variables back into a parameter vector. "
+function unshapeParams(w, alpha, eps)
+    return vcat(vec(w), alpha, eps)
+end
+
+" The ODE equations we're using. "
+function ODEeq(du, u, p, t)
+    w, alpha, eps = reshapeParams(p)
+    
+    du .= eps .* (1 .+ tanh.(w * u)) .- alpha .* u
+end
+
+" Solve the ODE system. "
+function solveODE(ps, tps=nothing)
+    u0 = zeros(83)
+
+    if isnothing(tps)
+        tspan = (0.0, 10000.0)
+    else
+        tspan = (0.0, maximum(tps))
+    end
+    
+    prob = ODEProblem(ODEeq, u0, tspan, ps)
+    sol = solve(prob, AutoTsit5(TRBDF2()); reltol=1e-8, abstol=1e-8)
+
+    if isnothing(tps)
+        return last(sol)
+    end
+
+    return sol(tps)
+end
+
+" Remove the effect of one gene across all others to simulate the KO experiments. "
+function simKO(pIn, geneNum)
+    pIn = copy(pIn) # Need to copy as we're using views
+    w_temp, alpha, eps = reshapeParams(pIn)
+    
+    # Think we remove a column since this is the effect of one gene across all genes
+    if geneNum == 1
+        w = hcat(zeros(Float64, 83, 1), w_temp[:, 2:83])
+    elseif geneNum == 83
+        w = hcat(w_temp[:, 1:82], zeros(Float64, 83, 1))
+    else
+        w= hcat(w_temp[:, 1:geneNum - 1], zeros(Float64, 83, 1), w_temp[:, geneNum + 1:83])
+    end
+    
+    pIn = unshapeParams(w, alpha, eps)
+    
+    return solveODE(pIn)
+end
+
+" Solves ODE system with given parameters to create comparable 83 x 84 matrix to experimental data. "
+function sol_matrix(pIn)
+    sol = ones(83, 84)
+    for i = 1:83
+        sol[:, i] = simKO(pIn, i)
+    end
+    sol[:, 84] = solveODE(pIn)
+    return sol
+end
+
+" Cost function. Returns SSE + sum(abs(w)) between model and experimental RNAseq data. "
+function cost(pIn)
+    exp_data = get_data("./de/data/exp_data.csv")
+    neg = solveODE(pIn)
+    sse = norm(neg .- exp_data[:, 84])
+
+    for i = 1:83
+        sol_temp = simKO(pIn, i)
+        sse += norm(sol_temp .- exp_data[:, i])
+    end
+
+    return sse + sum(abs.(pIn[1:6889])) # TODO: Add regularization strength param
+end
+
+" Calculates gradient of cost function. "
+function g!(G, x)
+    grads = Zygote.gradient(cost, x)
+    G[:] .= grads[1]
+end
+
+" Single calculation of cost gradient. "
+# ps = ones(7055)
+# grads = Zygote.gradient(cost, ps)
+
+" Run optimization. "
+#optimize(cost, g!, ps, LBFGS(), Optim.Options(iterations = 10, show_trace = true))
---
+++ de/py_model.py
@@ -0,0 +1,58 @@
+'''
+Basic Model simulation
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+
+def graph(cond, t, sol, save_path=""):
+    '''
+    Function receives condition name, time series, model solution, and path at which to save image.
+    Generates graphs of the solution over time.
+    '''
+    fig = plt.figure(figsize=(100, 300.0))
+    for i in range(83):
+        axes = fig.add_subplot(17, 5, i+1)
+        axes.plot(t, sol[:, i], 'r')
+        axes.set_title(cond)
+    plt.xlabel('t')
+    plt.ylabel('Expression Level')   # x(t)/x(0)
+    fig.tight_layout()
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+
+def random_params():
+    '''
+    Function receives simulation of ODE and randomly initialize the parameters based on the number of components.
+    Returns 1D array of parameters p = [eps (n_x), w (n_x ** 2), alpha (n_x)]
+    '''
+    eps = np.abs(np.random.normal(1, 1.0, size=(83)))
+    W = np.random.normal(0.01, 1.0, size=(83, 83))
+    W_mask = (1.0 - np.diag(np.ones([83]))) #remove self-interaction
+    w = W_mask*W
+    alpha = np.abs(np.random.normal(2, 1.0, size=(83)))
+    # Combine all parameters into p
+    p = np.concatenate([eps, w.flatten(), alpha])
+    return p
+
+def scatterplot(exp_data, model_data, save_path=""):
+    '''
+    Function receives matrix of RNAseq data, matrix of model solution, and the save path for image.
+    Creates scatterplot of model data vs experimental data
+    '''
+    rainbow = plt.get_cmap("rainbow")
+    cNorm = colors.Normalize(vmin=0, vmax=83)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=rainbow)
+    fig = plt.figure(figsize=(8, 8))
+    plt.subplot(1, 1, 1)
+    for i in range(83):
+        plt.scatter(exp_data[i, :], model_data[i, :], s=10, color=scalarMap.to_rgba(i))
+    plt.xlabel('RNAseq Data')
+    plt.ylabel('Model Solution')
+    plt.title("Model vs Data")
+    fig.tight_layout()
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
