63de5e4afe2ddb5de93f98d1ebe8c11a5e15c571
Aaron Meyer <git@ameyer.me>
2020-07-17 13:45:20 -0700

    Remove plotting tests.

--- de/tests/test_py_model.py
+++ de/tests/test_py_model.py
@@ -6,7 +6,7 @@ In addition, the relative concentration of knockout component should approach ze
 '''
 import unittest
 import numpy as np
-from ..py_model import graph, random_params, scatterplot
+from ..py_model import random_params
 
 class TestPyModel(unittest.TestCase):
     '''Test class for basic simulation of ODE systems'''
@@ -16,19 +16,3 @@ class TestPyModel(unittest.TestCase):
         '''
         p = random_params()
         self.assertEqual(np.shape(p), (83 ** 2 + 2 * 83, ))
-
-    def test_graph(self):
-        '''
-        Test the graph function
-        '''
-        t_test = np.arange(100)
-        sol_test = np.random.normal(0.01, 1.0, size=(100, 83))
-        graph("random test", t_test, sol_test)
-
-    def test_scatterplot(self):
-        '''
-        Test the scatterplot function
-        '''
-        exp_rand = np.random.normal(0.01, 1.0, size=(83, 84))
-        model_rand = np.random.normal(0.01, 1.0, size=(83, 84))
-        scatterplot(exp_rand, model_rand)
