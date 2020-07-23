db0d77b55295309adf1883a092d1111961138a64
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-07-23 10:05:24 -0700

    Some various linting. (#46)
    Co-authored-by: Aaron Meyer <git@ameyer.me>

--- de/figures/figure1.py
+++ de/figures/figure1.py
@@ -6,29 +6,31 @@ from .figureCommon import subplotLabel, getSetup
 from ..PCA_helpers import performPCA, KOdataframe
 from ..importData import prepData
 
+
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
     ax, f = getSetup((15, 4), (1, 3))
-    
+
     # Perform PCA
     data = prepData()
     pca_object, X_r = performPCA(data.T, 25)
-    
+
     # Create dataframe for plotting
     df, KO_genes_unique = KOdataframe(data, X_r)
-    
+
     # Make subplots
     figureMaker(ax, pca_object, df, KO_genes_unique)
-    
+
     # Add subplot labels
     subplotLabel(ax)
 
     return f
 
+
 def figureMaker(ax, pca_object, df, KO_genes_unique):
     """ Makes 3 panels: (A) R2X, (B) PC2 vs PC1, (C) ? """
-    
+
     # Plot R2X
     i = 0
     total_variance = np.array([])
@@ -41,7 +43,7 @@ def figureMaker(ax, pca_object, df, KO_genes_unique):
     ax[i].set_xticks(np.arange(1, 26, 2))
     ax[i].plot(list(range(1, 26)), total_variance)
     ax[i].set_title("R2X")
-    
+
     # Plot PC2 vs PC1
     i += 1
     KO_genes = df.loc[:, "KO Gene"]
@@ -50,10 +52,10 @@ def figureMaker(ax, pca_object, df, KO_genes_unique):
         ax[i].scatter(df.iloc[:, 0][indx], df.iloc[:, 1][indx], s=8)
     for j, txt in enumerate(KO_genes):
         ax[i].annotate(txt, (df.iloc[j, 0], df.iloc[j, 1]), fontsize=6)
-    ax[i].set_xlabel("PC1 (" + str(round(pca_object.explained_variance_ratio_[0]*100, 2)) + "%)")
-    ax[i].set_ylabel("PC2 (" + str(round(pca_object.explained_variance_ratio_[1]*100, 2)) + "%)")
+    ax[i].set_xlabel("PC1 (" + str(round(pca_object.explained_variance_ratio_[0] * 100, 2)) + "%)")
+    ax[i].set_ylabel("PC2 (" + str(round(pca_object.explained_variance_ratio_[1] * 100, 2)) + "%)")
     ax[i].set_title("PC2 vs PC1")
-    
+
     # Plot PC3 vs PC1
     i += 1
     KO_genes = df.loc[:, "KO Gene"]
@@ -62,6 +64,6 @@ def figureMaker(ax, pca_object, df, KO_genes_unique):
         ax[i].scatter(df.iloc[:, 0][indx], df.iloc[:, 2][indx], s=8)
     for j, txt in enumerate(KO_genes):
         ax[i].annotate(txt, (df.iloc[j, 0], df.iloc[j, 2]), fontsize=6)
-    ax[i].set_xlabel("PC1 (" + str(round(pca_object.explained_variance_ratio_[0]*100, 2)) + "%)")
-    ax[i].set_ylabel("PC3 (" + str(round(pca_object.explained_variance_ratio_[2]*100, 2)) + "%)")
+    ax[i].set_xlabel("PC1 (" + str(round(pca_object.explained_variance_ratio_[0] * 100, 2)) + "%)")
+    ax[i].set_ylabel("PC3 (" + str(round(pca_object.explained_variance_ratio_[2] * 100, 2)) + "%)")
     ax[i].set_title("PC3 vs PC1")
