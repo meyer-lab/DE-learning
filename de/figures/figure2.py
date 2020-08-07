c7745bb00c6a1755e021c6a10388e9cd869f12e3
Zhanzhang-2020 <11712523@mail.sustech.edu.cn>
2020-08-07 17:31:57 +0800

    edits

--- de/figures/figure2.py
+++ de/figures/figure2.py
@@ -2,6 +2,7 @@
 This creates Figure 2: w Network Graph
 """
 import networkx as nx
+import numpy as np
 from .figureCommon import subplotLabel, getSetup
 from ..graph import load_w, add_nodes, add_edges
 
@@ -14,12 +15,19 @@ def makeFigure():
     G = nx.DiGraph()
     w = load_w()
     add_nodes(G, w)
-    add_edges(G, w)
+    G, threshold, w_max = add_edges(G, w)
     ax[0].set_title("w Network Graph")
     labels = nx.get_node_attributes(G, "gene")
-    nx.draw_networkx(G, labels=labels, node_size=200, font_size=8)
+    pos = nx.spring_layout(G, seed=0)
+    
+    #adjust edges color and thickness based on interaction type and weights
+    edges = G.edges()
+    colors = [G[u][v]["color"] for u,v in edges]
+    weights = [np.exp((abs(G[u][v]['weight']) - threshold) / (w_max - threshold)) for u,v in edges]
+    
+    nx.draw_networkx(G, pos, labels=labels, edges=edges, edge_color=colors, width=weights, node_size=300, font_size=8)
 
     # Add subplot labels
     subplotLabel(ax)
-
+                               
     return f
--- de/graph.py
+++ de/graph.py
@@ -3,7 +3,6 @@ from os.path import join, dirname
 import numpy as np
 import pandas as pd
 import networkx as nx
-import matplotlib.pyplot as plt
 
 def load_w():
     """
@@ -29,12 +28,17 @@ def add_edges(dir_graph, w):
     Given a directed graph and w matrix, calculates a threshold for large w values. Then adds a directed edge from gene j to gene i representing the interaction with the w value as the edge's weight.
     """
     w = w.to_numpy()
-    threshold = np.mean(w) + 1.25 * np.std(w)
+    w_new = abs(w)
+    w_max = np.max(w_new)
+    threshold = np.mean(w_new) + 1.5 * np.std(w_new)
     for i in range(83):
         for j in range(83):
-            if w[i, j] > threshold:
-                dir_graph.add_edge(j, i, weight=w[i, j])
+            if w_new[i, j] > threshold:
+                if w[i, j] > 0:
+                    dir_graph.add_edge(j, i, color="red", weight=w[i, j])
+                else:
+                    dir_graph.add_edge(j, i, color="blue", weight=w[i, j])
     # Remove nodes with no edges
     isolates = list(nx.isolates(dir_graph))
     dir_graph.remove_nodes_from(isolates)
-    return dir_graph
+    return dir_graph, threshold, w_max
