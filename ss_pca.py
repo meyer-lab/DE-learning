37bfa3f014006c146a24f076d58e370323bd676b
Madison Wahlsten <mlwahl@umich.edu>
2020-08-31 17:28:54 -0700

    pca of model solves over time

---
+++ ss_pca.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.decomposition import PCA
+
+ss_vals = np.loadtxt("steady_state_values.csv", delimiter=',')
+
+pca_object = PCA(n_components=2)
+X_r = pca_object.fit_transform(ss_vals)
+loadings = pca_object.components_
+
+ax, f = getSetup((8, 8), (1, 1))
+
+i = 0
+ax[i].scatter(X_r[:, 0], X_r[:, 1])
+ax[i].set_xlabel("PC1 (" + str(round(pca_object.explained_variance_ratio_[0] * 100, 2)) + "%)")
+ax[i].set_ylabel("PC2 (" + str(round(pca_object.explained_variance_ratio_[1] * 100, 2)) + "%)")
+ax[i].set_title("PC2 vs PC1")
+
+for x in range(10):
+    time_sols = np.loadtxt("de/data/time_sol" + str(x + 1) + ".csv", delimiter=',')
+    t_scores = np.matmul(loadings, time_sols)
+    ax[i].plot(t_scores[0, 0:999], t_scores[1, 0:999])
