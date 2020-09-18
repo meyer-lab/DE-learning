fix normalization

de/figures/figure2.py
@@ -3,7 +3,7 @@ This creates Figure 2: w Network Graph
 """
 import numpy as np
 from .figureCommon import subplotLabel, getSetup
-from ..graph import Network, load_w, remove, bar_graph
+from ..graph import Network, load_w, normalize, remove, bar_graph
 
 
 def makeFigure():
@@ -12,6 +12,7 @@ def makeFigure():
     ax, f = getSetup((12, 12), (2, 2))
     #load w
     w = load_w()
+    w = normalize(w)
     w = remove(w)
     #Plot downstream graph
     w_abs = np.absolute(w.to_numpy())