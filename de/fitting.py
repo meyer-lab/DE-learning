fa647122d6d4781f960e6f91740dcff5ce50e5ac
Aaron Meyer <ameyer@ucla.edu>
2020-12-29 11:22:13 -0800

    Fitting now working

--- de/fitting.py
+++ de/fitting.py
@@ -1,16 +1,22 @@
-from jax import grad, jit
+import numpy as np
+from jax import grad, jit, jvp
 import jax.numpy as jnp
-from jax.nn import softplus
+from jax.config import config
+from scipy.optimize import minimize
+from .factorization import alpha, factorizeEstimate
+
+config.update("jax_enable_x64", True)
 
 
-def reshapeParams(p):
+def reshapeParams(p, nGenes):
     """Reshape a vector of parameters into the variables we know."""
-    w = jnp.reshape(p[1:6889], (83, 83))
-    eta = p[6890:6972]
+    nKO = int(p.size / nGenes - 1)
+    w = jnp.reshape(p[:(nKO * nGenes)], (nGenes, nKO))
+    eta = p[-nGenes:]
 
     assert eta.size == w.shape[0]
 
-    return w, softplus(eta)
+    return w, eta
 
 
 def cost(pIn, data, U = None):
@@ -19,29 +25,36 @@ def cost(pIn, data, U = None):
         U = np.copy(data)
         np.fill_diagonal(U, 0.0)
 
-    w, eta = reshapeParams(pIn)
+    w, eta = reshapeParams(pIn, data.shape[0])
     costt = jnp.linalg.norm(eta * (1 + jnp.tanh(w @ U)) - alpha * data)
-    costt += regularize(pIn)
+    costt += regularize(pIn, data.shape[0])
 
     return costt
 
 
-cost_grad = grad(cost, argnums=0)
-
-
-def regularize(pIn, strength = 1.0):
+def regularize(pIn, nGenes, strength = 1.0):
     """Calculate the regularization."""
-    w = reshapeParams(pIn)[0]
+    w = reshapeParams(pIn, nGenes)[0]
 
     ll = jnp.linalg.norm(w, ord=1)
     ll += jnp.linalg.norm(w.T @ w - jnp.identity(w.shape[0]))
     return strength * ll
 
 
-def costG(x, data):
-    """ Cost function gradient. """
-    U = jnp.copy(data)
-    jnp.fill_diagonal(U, 0.0)
-    G = cost_grad(x, data, U)
-    return G
+def runOptim(data, niter = 2000, disp = False):
+    """ Run the optimization. """
+    # TODO: Add bounds to fitting.
+    w, eps = factorizeEstimate(data)
+    x0 = np.concatenate((w.flatten(), eps))
 
+    U = np.copy(data)
+    np.fill_diagonal(U, 0.0)
+    cost_grad = jit(grad(cost, argnums=0))
+
+    def hvp(x, v, data, U):
+        return grad(lambda x: jnp.vdot(cost_grad(x, data, U), v))(x)
+
+    res = minimize(cost, x0, args=(data, U), method="trust-krylov", jac=cost_grad, hessp=hvp, options={"maxiter": niter, "disp": disp})
+    assert (res.success == True) or (res.nit == niter)
+
+    return res.x
