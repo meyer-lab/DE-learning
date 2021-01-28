3b96fc8006be7501b58bef180ffa5a6b08017a29
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-01-28 08:32:22 -0800

    Update factorization.py

--- de/factorization.py
+++ de/factorization.py
@@ -6,11 +6,8 @@ from scipy.special import expit, logit
 alpha = 0.1
 
 
-def calcW(data, eta, alpha):
+def calcW(data, U, eta, alpha):
     """Directly calculate w."""
-    U = np.copy(data)
-    np.fill_diagonal(U, 0.0)
-
     B = (data * alpha) / eta[:, np.newaxis]
     assert np.all(np.isfinite(B))
     B = logit(np.clip(B, 0.0001, 0.9999))
@@ -18,9 +15,9 @@ def calcW(data, eta, alpha):
     return np.linalg.lstsq(U.T, B.T, rcond=None)[0].T
 
 
-def calcEta(data, w, alpha):
+def calcEta(data, U, w, alpha):
     """Directly calculate eta."""
-    eta = (alpha * data) / expit(w @ data)
+    eta = (alpha * data) / expit(w @ U)
     eta = np.nan_to_num(eta, nan=1.0, posinf=1e9)
     eta = np.clip(eta, 1e-9, 1e9)
     return gmean(eta, axis=1)
@@ -37,10 +34,10 @@ def factorizeEstimate(data, tol=1e-9, maxiter=10000):
 
     # Use the data to try and initialize the parameters
     for ii in range(maxiter):
-        eta = calcEta(data, w, alpha)
+        eta = calcEta(data, U, w, alpha)
         assert np.all(np.isfinite(eta))
         assert eta.shape == (data.shape[0], )
-        w = calcW(data, eta, alpha)
+        w = calcW(data, U, eta, alpha)
         assert np.all(np.isfinite(w))
 
         assert w.shape == (data.shape[0], data.shape[0])
