dbd70e3d2563cee7cda4e00b4a4e84a41fafd25b
farnazmdi <farnazmd95@gmail.com>
2021-05-12 22:28:10 -0700

    some re-arrangements

--- de/factorization.py
+++ de/factorization.py
@@ -64,3 +64,20 @@ def cellLineFactorization(cellLine):
     data, annotation = importLINCS(cellLine)
     w, eta = factorizeEstimate(data)
     return w, eta, annotation[0].tolist()
+
+def cellLineComparision(cellLine1, cellLine2):
+    w1, eta1, annotation1 = cellLineFactorization(cellLine1)
+    w2, eta2, annotation2 = cellLineFactorization(cellLine2)
+
+    line1_as_set = set(annotation1)
+    intersection = line1_as_set.intersection(annotation2)
+    intersection_annotation = list(intersection)
+
+    index_list1 = []
+
+    for x in intersection_annotation:
+        index_value1 = annotation1.index(x)
+        index_list1.append(index_value1)
+
+    index_list1.sort()
+    return index_list1
