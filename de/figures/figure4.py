7af11d98d8d83c611f9f4a0fae3c8abe37391e27
Angela Liu <angelal@aretha.seas.ucla.edu>
2021-06-28 12:42:32 -0700

    spread out and color coded nodes

--- de/figures/figure4.py
+++ de/figures/figure4.py
@@ -1,8 +1,10 @@
 """
-This creates Figure 3
+This creates Figure 4
 """
 from .figureCommon import subplotLabel, getSetup
 
+above = ["JUN", "BRD2", "STK11", "PKN2", "NFAT5", "KMT2D", "ADCK3", "FOSL1", "CSK", "BRD8", "CBFB", "TADA2B", "DSTYK", "JUNB", "LATS2", "FEZF2", "MITF", "RUNX3", "SUV420H1", "SOX10", "DOT1L", "PRKRIR"] 
+below = ["MAP3K1", "MAP2K7", "NSD1", "KDM1A", "EGFR", "EP300", "SRF", "PRKAA1", "GATA4", "MYBL1", "MTF1"]
 
 def Network(w, w_abs, w_max, ax):
     """
@@ -13,19 +15,16 @@ def Network(w, w_abs, w_max, ax):
     add_nodes(G, w, w_abs)
     add_edges(G, w, w_abs)
     remove_isolates(G)
-
-    pos = nx.nx_pydot.pydot_layout(G, prog="fdp")
-
+    pos = nx.nx_pydot.pydot_layout(G, prog="fdp", scale = 3)
     # draw the nodes, edges and labels
     set_nodes(G, pos, ax)
     set_edges(G, w_abs, w_max, pos, ax)
     set_labels(G, pos, ax)
-    # determines node color based on 
+    # determines node color 
     color_map = []
     for node in G: 
-        if node > x=y:
+        if 'set_labels' in above :
             color_map.append('red')
-        else:
+        else if 'set_labels' in below:
             color_map.append('blue')
-
     return G
