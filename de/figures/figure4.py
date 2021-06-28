086257b5773c9b4069176b34fc4b0c371b0a7fa7
Angela Liu <angelal@aretha.seas.ucla.edu>
2021-06-28 09:34:56 -0700

    node color depends on type

--- de/figures/figure4.py
+++ de/figures/figure4.py
@@ -4,12 +4,28 @@ This creates Figure 3
 from .figureCommon import subplotLabel, getSetup
 
 
-def makeFigure():
-    """ Get a list of the axis objects and create a figure. """
-    # Get list of axis objects
-    ax, f = getSetup((15, 8), (3, 2))
+def Network(w, w_abs, w_max, ax):
+    """
+    Given w, w_abs, w_max and ax, then draw the corresponding Networkx graph.
+    """
+    G = nx.DiGraph()
+    # add nodes and edges
+    add_nodes(G, w, w_abs)
+    add_edges(G, w, w_abs)
+    remove_isolates(G)
 
-    # Add subplot labels
-    subplotLabel(ax)
+    pos = nx.nx_pydot.pydot_layout(G, prog="fdp")
 
-    return f
+    # draw the nodes, edges and labels
+    set_nodes(G, pos, ax)
+    set_edges(G, w_abs, w_max, pos, ax)
+    set_labels(G, pos, ax)
+    # determines node color based on 
+    color_map = []
+    for node in G: 
+        if node > x=y:
+            color_map.append('red')
+        else:
+            color_map.append('blue')
+
+    return G
