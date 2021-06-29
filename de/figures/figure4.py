5e5c0bb00e1c2fb3915aca0cdc35693c9ad77026
Angela Liu <angelal@aretha.seas.ucla.edu>
2021-06-29 13:47:56 -0700

    node edit

--- de/figures/figure4.py
+++ de/figures/figure4.py
@@ -1,12 +1,14 @@
 """
 This creates Figure 4
 """
+from .graph import add_nodes, add_edges, remove_isolates, set_edges, set_nodes, set_labels
 from .figureCommon import subplotLabel, getSetup
+import networkx as nx
 
 above = ["JUN", "BRD2", "STK11", "PKN2", "NFAT5", "KMT2D", "ADCK3", "FOSL1", "CSK", "BRD8", "CBFB", "TADA2B", "DSTYK", "JUNB", "LATS2", "FEZF2", "MITF", "RUNX3", "SUV420H1", "SOX10", "DOT1L", "PRKRIR"] 
 below = ["MAP3K1", "MAP2K7", "NSD1", "KDM1A", "EGFR", "EP300", "SRF", "PRKAA1", "GATA4", "MYBL1", "MTF1"]
 
-def Network(w, w_abs, w_max, ax):
+def Networkedit(w, w_abs, w_max, ax):
     """
     Given w, w_abs, w_max and ax, then draw the corresponding Networkx graph.
     """
@@ -19,6 +21,7 @@ def Network(w, w_abs, w_max, ax):
     # draw the nodes, edges and labels
     set_nodes(G, pos, ax)
     set_edges(G, w_abs, w_max, pos, ax)
+    labels = nx.get_node_attributes(G, "gene")
     set_labels(G, pos, ax)
     #set title for graph
     ax[0].set_title("w Network Graph (downstream)")
