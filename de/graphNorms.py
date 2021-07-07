650ee0c9fa03f1dd803b98c9806ab7b479654ccf
Myra Dada <40546509+mdada1@users.noreply.github.com>
2021-07-07 13:29:40 -0700

    Added function to plot correlation graphs

--- de/graphNorms.py
+++ de/graphNorms.py
@@ -1,4 +1,4 @@
-from .fitting import cellLineFactorization, MatrixSubtraction
+from .factorization import cellLineFactorization, MatrixSubtraction
 import matplotlib.pyplot as plt
 import numpy as np
 from math import ceil
@@ -62,4 +62,38 @@ def plot_entries_scatterplot(cellLine1, cellLine2):
     plt.scatter(w1_edit, w2_edit, marker = 'o')
     plt.title("Flattened Matrice Graph")
     plt.savefig('w1_w2graph.png')
+
+def plot_corr_graphs(cell_lines):
+    """Plot all combinations of two w matrices against each other (still using factorizeEstimate instead of runOptim)"""
+
+    ncols = 3
+    nplots = len(cell_lines)*(len(cell_lines)-1)/2
+    nrows = ceil(nplots/ncols)
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(7.5*ncols, 6*nrows), squeeze=0, sharex=False, sharey=True)
+    axes = np.array(axes)
+
+    labels = []
+    data = []
+
+    for i in range(0,len(cell_lines)-1):
+        for j in range(i+1,len(cell_lines)):
+            cellLine1 = cell_lines[i]
+            cellLine2 = cell_lines[j]
+            label_list = [cellLine1, cellLine2]
+            labels.append(label_list)
+            _, _, w1, w2 = MatrixSubtraction(cellLine1, cellLine2)
+            data.append([w1.flatten(), w2.flatten()])
+
+
+    for i, ax in enumerate(axes.reshape(-1)):
+        label_list = labels[i]
+        values = data[i]
+        ax.set_title(label_list[1] + ' vs. ' + label_list[0])
+        ax.scatter(values[0], values[1])
+        ax.axhline(0, color="black")
+        ax.axvline(0, color="black")
+        ax.set_xlabel(label_list[0])
+        ax.set_ylabel(label_list[1])
+
+    plt.savefig('corr_graphs.png')
 
