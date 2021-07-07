47c79153f5d277787c7a27fe2e7238bda38e7fe4
Angela Liu <angelal@aretha.seas.ucla.edu>
2021-07-07 11:11:20 -0700

    full melanoma gene list

--- de/newgenes.py
+++ de/newgenes.py
@@ -3,9 +3,28 @@ from os.path import join, dirname
 import numpy as np
 import pandas as pd
 
-def importTorre():  
-    """ Imports all Torre genes. """
+def importmelanoma(cellLine):  
+    """ Imports all Torre genes with Fig 5D data"""
     path_here = dirname(dirname(__file__))
-    data = pd.read_csv(join(path_here, "de/data/torre_metadata.txt"), header = None, index_col="geneName", sep='', dtype = None)
-    data = data[:,[0,2,3]]
+    xdata = pd.read_csv(join(path_here, "de/data/sumarizedResults.txt"), header = True, index_col="geneName", sep='', dtype = None)
+    ydata = pd.read_csv(join(path_here, "de/data/colonyGrowthResults_allhits.txt", header = True, index_col="geneName", sep = '', dtype = None))
+    xdata = xdata[:,[0,4]]
+    ydata = xdata[:,[0,2]]
+    return xdata, ydata
+
+def mergedData(xdata, ydata):
+    data = pd.merge(xdata, ydata, on='target',how='inner')
     return data
+
+def splitnodes(data):
+    above = [""]
+    below = [""]
+    for key, data[0] in data:
+        if data[1] < data[2]:
+            above.append(data[0])
+            return above
+        elif data[1] > data[2]:
+            below.append(data[0])
+            return below 
+        
+
