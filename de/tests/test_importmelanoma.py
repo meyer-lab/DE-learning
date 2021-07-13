260b7d03147ebf79c0ed3736e1d6cb1fb6f65ba0
Angela Liu <angelal@aretha.seas.ucla.edu>
2021-07-13 13:03:34 -0700

    error linting

--- de/tests/test_importmelanoma.py
+++ de/tests/test_importmelanoma.py
@@ -1,16 +1,17 @@
 import unittest
 import numpy as np
+import pandas as pd
 from ..importData import importmelanoma
 
 class TestModel(unittest.TestCase):
     def test_merge(self):
         """Tests that merged DataFrame has three columns: target, meanlFC, and Rcolonies_lFC"""
         matrix = importmelanoma()
-        self.assertTrue(isinstance(matrix, np.ndarray))
+        self.assertTrue(isinstance(matrix, pd.DataFrame))
         self.assertEqual(matrix.shape[1], 3)
         assert len(matrix) <= 88 #merged matrix has fewer genes than input files
 
-    def test_overlap():
+    def test_overlap(self):
         """To test and confirm all targets/genes are found in both x_data and y_data"""
         x_data = importmelanoma()
         y_data = importmelanoma()
