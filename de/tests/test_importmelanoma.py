1d28228ec1d477f1436719bba8570efeca939c62
Angela Liu <angelal@aretha.seas.ucla.edu>
2021-07-13 11:29:19 -0700

    melanoma test

---
+++ de/tests/test_importmelanoma.py
@@ -0,0 +1,18 @@
+import numpy as np
+from ..importData import importmelanoma
+
+def test_merge(self):
+    """Tests that merged DataFrame has three columns: target, meanlFC, and Rcolonies_lFC"""
+    matrix = importmelanoma()
+    self.assertTrue(isinstance(matrix, np.ndarray))
+    self.assertEqual(matrix.shape[1], 3)
+    assert len(matrix) <= 88 #merged matrix has fewer genes than input files
+
+def test_overlap():
+    """To test and confirm all targets/genes are found in both x_data and y_data"""
+    x_data = importmelanoma()
+    y_data = importmelanoma()
+    _, _, annotation1 = x_data
+    _, _, annotation2 = y_data
+    data = importmelanoma()
+    assert np.abs(len(data)) == np.min([len(annotation1), len(annotation2)])
