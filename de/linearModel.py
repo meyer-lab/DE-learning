0e6f4b02c4c9b9703d9e34123dda1f915ae131dc
AshlynPowell <ashlynpowell913@gmail.com>
2021-08-10 11:15:57 -0700

    linting

--- de/linearModel.py
+++ de/linearModel.py
@@ -3,29 +3,24 @@ from .importData import ImportMelanoma, importLINCS
 import numpy as np
 
 def runFitting(data, U=None, max_iter=300000):
-    #cellLines = ["A375", "A549", "HA1E", "HT29", "MCF7", "PC3"]
+    """Creates Lasso object, fits model to data"""
     
     if U is None:
         U = np.copy(data)
         np.fill_diagonal(U, 0.0)
-
     model = Lasso(max_iter=max_iter)
     model.fit(U, data)
-
     p = model.predict(U)
-
     cost = SSE(data, U, model)
     print(f"cost: {cost}")
-
-    #np.savetxt(f"{cellLine}.csv", model.coef_, delimiter=",")
 
     return model.coef_, p
 
 def SSE(data, U, model):
+    """Calculates the SSE of the model"""
 
     p = model.predict(U)
     diff = np.absolute(data - p)
-    
     square = np.power(diff,2)
     sum_errors = np.sum(square)
     
