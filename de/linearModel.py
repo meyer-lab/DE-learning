9153b9441b7cea07c26c3faba80ca0d038b48b18
AshlynPowell <ashlynpowell913@gmail.com>
2021-08-16 15:59:25 -0700

    regularization parameter

--- de/linearModel.py
+++ de/linearModel.py
@@ -1,13 +1,13 @@
 from sklearn.linear_model import Lasso
 import numpy as np
 
-def runFitting(data, U=None, max_iter=300000):
+def runFitting(data, U=None, max_iter=300000, alpha=1.0):
     """Creates Lasso object, fits model to data"""
     
     if U is None:
         U = np.copy(data)
         np.fill_diagonal(U, 0.0)
-    model = Lasso(max_iter=max_iter)
+    model = Lasso(max_iter=max_iter, alpha=alpha)
     model.fit(U, data)
 
     return model
