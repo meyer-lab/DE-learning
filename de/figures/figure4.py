a37ade8da7a1279db39f3371ad8aa85b7862a1b8
farnazmdi <farnazmd95@gmail.com>
2021-09-01 08:27:02 -0700

    figure dimensions

--- de/figures/figure4.py
+++ de/figures/figure4.py
@@ -7,9 +7,9 @@ from .figureCommon import subplotLabel, getSetup
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
-    ax, f = getSetup((15, 8), (3, 2))
+    ax, f = getSetup((80, 50), (3, 2))
 
     # Add subplot labels
-    subplotLabel(ax)
+    subplotLabel(ax, fntsize=50)
 
     return f
