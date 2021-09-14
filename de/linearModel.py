6039ea9136857661ab236dc0245744a676b29a31
Aaron Meyer <ameyer@ucla.edu>
2021-09-14 09:26:00 -0700

    Upgrade packages

--- de/linearModel.py
+++ de/linearModel.py
@@ -1,13 +1,13 @@
 from sklearn.linear_model import Lasso
 import numpy as np
 
-def runFitting(data, U=None, max_iter=300000, alpha=1.0):
-    """Creates Lasso object, fits model to data"""
-    
+def runFitting(data, U=None, alpha=1.0):
+    """ Creates Lasso object, fits model to data. """
     if U is None:
         U = np.copy(data)
         np.fill_diagonal(U, 0.0)
-    model = Lasso(max_iter=max_iter, alpha=alpha)
+
+    model = Lasso(max_iter=300000, alpha=alpha)
     model.fit(U, data)
 
     return model
