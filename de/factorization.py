bab3e8b4599ca4c3f316e3ec5f091bcce1303961
farnazm <farnazmd95@gmail.com>
2021-11-15 15:50:50 -0800

    lambda 10

--- de/factorization.py
+++ de/factorization.py
@@ -48,7 +48,7 @@ def calcW(data: list, eta: list, alphaIn: float) -> np.ndarray:
             U = np.concatenate((U, U1), axis=1)
             B = np.concatenate((B, B1), axis=1)
 
-    lamb = 1.0
+    lamb = 10.0
     # return np.linalg.lstsq(U.T, B.T, rcond=None)[0].T
     return np.linalg.lstsq(U.dot(U.T) + lamb * np.identity(U.T.shape[1]), U.dot(B.T))[0].T
 
