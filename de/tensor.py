7ff3c74f338007801c7d1000de92bff38fa85e63
farnazm <farnazmd95@gmail.com>
2022-02-02 10:41:33 -0800

    form tensor addedg

--- de/tensor.py
+++ de/tensor.py
@@ -1,12 +1,13 @@
 """ Organizing the data into tensor format. """
 import numpy as np
 from scipy.stats import zscore
-
+import tensorly as tl
+from tensorly.decomposition import parafac
 from .importData import importLINCS
 from .factorization import commonGenes
 
 
-def tensor() -> np.ndarray:
+def form_tensor() -> np.ndarray:
     """ find all common genes and create a tensor format data from genes, cell lines, and gene expression levels. """
     # import
     A375, gA375 = importLINCS("A375")
@@ -20,26 +21,39 @@ def tensor() -> np.ndarray:
     ids = commonGenes([gA375, gA549, gHA1E, gHT29, gMCF7, gPC3])
     n = ids[0].shape[0]
 
-    Tensor = np.zeros((n, n+1, len(ids))) # the added condition in the second dimension is the control
+    Tensor = tl.zeros((n, n+1, len(ids))) # the added condition in the second dimension is the control
 
     # only keep common genes
     A37 = A375[ids[0], :]
-    Tensor[:, :, 0] = A37[:, np.append(ids[0], [-1])]
+    Tensor[:, :n, 0] = A37[:, ids[0]]
     A54 = A549[ids[1], :]
-    Tensor[:, :, 1] = A54[:, np.append(ids[1], [-1])]
+    Tensor[:, :n, 1] = A54[:, ids[1]]
     HA1 = HA1E[ids[2], :]
-    Tensor[:, :, 2] = HA1[:, np.append(ids[2], [-1])]
+    Tensor[:, :n, 2] = HA1[:, ids[2]]
     HT2 = HT29[ids[3], :]
-    Tensor[:, :, 3] = HT2[:, np.append(ids[3], [-1])]
+    Tensor[:, :n, 3] = HT2[:, ids[3]]
     MCF = MCF7[ids[4], :]
-    Tensor[:, :, 4] = MCF[:, np.append(ids[4], [-1])]
+    Tensor[:, :n, 4] = MCF[:, ids[4]]
     PC = PC3[ids[5], :]
-    Tensor[:, :, 5] = PC[:, np.append(ids[5], [-1])]
+    Tensor[:, :n, 5] = PC[:, ids[5]]
+
+    # controls
+    Tensor[:, n, 0] = A375[ids[0], -1]
+    Tensor[:, n, 1] = A549[ids[1], -1]
+    Tensor[:, n, 2] = HA1E[ids[2], -1]
+    Tensor[:, n, 3] = HT29[ids[3], -1]
+    Tensor[:, n, 4] = MCF7[ids[4], -1]
+    Tensor[:, n, 5] = PC3[ids[5], -1]
+
+    # zscore each gene
+    for i in range(Tensor.shape[2]):
+        Tensor[:, :, i] = zscore(Tensor[:, :, i], axis=0)
 
     # assert the genes are the same among cell line1 and 2
     assert(np.all(np.array(gA375)[ids[0]] == np.array(gA549)[ids[1]]))
-
     gene_names = np.array(gA375)[ids[0]]
-    np.append(gene_names, ['Control'])
 
-    return zscore(Tensor, axis=1), gene_names
+    cellLines = ["A375", "A549", "HA1E", "HT29", "MCF7", "PC3"]
+
+    return Tensor, gene_names, cellLines
+
