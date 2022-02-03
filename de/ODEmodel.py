903b0e650e69d7606fffbf20572cc151eec77ffd
farnazm <farnazmd95@gmail.com>
2022-02-03 10:11:13 -0800

    ode model back from last yeat

---
+++ de/ODEmodel.py
@@ -0,0 +1,73 @@
+"""
+This file contains ODE equation solvers
+"""
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+
+
+def solver(ps, ts):
+    '''Function receives time series and a set of parameters,
+       then return the simulation of ODE.
+    '''
+    w = np.reshape(ps[:6889], (83, 83))
+    alpha = ps[6889:6972]
+    eps = ps[6972:]
+
+    assert alpha.size == w.shape[0]
+    assert eps.size == w.shape[0]
+    assert w.shape[1] == w.shape[0]
+
+    x0 = eps / alpha
+    sol = odeint(ODE, x0, ts, args=(eps, w, alpha))
+    return sol
+
+
+def ODE(y, _, eps, w, alpha):
+    '''The ODE system:
+    Parameters = eps: Value that bound the saturation effect
+                 w: Interaction between components
+                 alpha: Degradation rate
+    '''
+    return eps * np.tanh(np.dot(w, y)) - alpha * y
+
+
+def graph(cond, t, sol, save_path=""):
+    """
+    Function receives condition name, time series, model solution, and path at which to save image.
+    Generates graphs of the solution over time.
+    """
+    fig = plt.figure(figsize=(100, 300.0))
+    for i in range(83):
+        axes = fig.add_subplot(17, 5, i + 1)
+        axes.plot(t, sol[:, i], "r")
+        axes.set_title(cond)
+    plt.xlabel("t")
+    plt.ylabel("Expression Level")  # x(t)/x(0)
+    fig.tight_layout()
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+
+
+def scatterplot(exp_data, model_data, save_path=""):
+    """
+    Function receives matrix of RNAseq data, matrix of model solution, and the save path for image.
+    Creates scatterplot of model data vs experimental data
+    """
+    rainbow = plt.get_cmap("rainbow")
+    cNorm = colors.Normalize(vmin=0, vmax=83)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=rainbow)
+    fig = plt.figure(figsize=(8, 8))
+    plt.subplot(1, 1, 1)
+    for i in range(83):
+        plt.scatter(exp_data[i, :], model_data[i, :], s=10, color=scalarMap.to_rgba(i))
+    plt.xlabel("RNAseq Data")
+    plt.ylabel("Model Solution")
+    plt.title("Model vs Data")
+    fig.tight_layout()
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
