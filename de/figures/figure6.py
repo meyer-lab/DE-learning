3100e1dfcd0cb0f33e12b7356887149a0250cf20
farnazm <farnazmd95@gmail.com>
2022-03-22 19:22:48 -0700

    better

--- de/figures/figure6.py
+++ de/figures/figure6.py
@@ -45,68 +45,45 @@ def ODE(y, _, eta, w):
 
     return eta * expit(np.dot(w, y)) - alpha * y
 
-def makeFigure():
+def network_odesol(ax, cellLine:str, list_gene:list):
+    """ Given the list of genes and the cell line of interest plots the ode response and network. """
 
-    ax, f = getSetup((6, 12), (5, 2))
-    melanoma_gene_list = ['FOSL1', 'FOXC2', 'JUN', 'JUNB', 'neg']
-    A375_gene_list = ['FOS', 'FOSL1', 'FOXJ3', 'FOXO3', 'FOXO4', 'JUN', 'control']
-
-    w_mel, eta_mel = import_factorize('Melanoma', melanoma_gene_list)
-    w_375, eta_375 = import_factorize('A375', A375_gene_list)
-    w_549, eta_549 = import_factorize('A549', A375_gene_list)
-    w_ha1e, eta_ha1e = import_factorize('HA1E', A375_gene_list)
-    w_ht29, eta_ht29 = import_factorize('HT29', A375_gene_list)
+    # run factorization
+    w, eta = import_factorize(cellLine, list_gene)
 
     # plot network
-    Network(pd.DataFrame(w_mel, index=melanoma_gene_list[:-1], columns=melanoma_gene_list[:-1]), ax[0])
-    Network(pd.DataFrame(w_375, index=A375_gene_list[:-1], columns=A375_gene_list[:-1]), ax[2])
-    Network(pd.DataFrame(w_549, index=A375_gene_list[:-1], columns=A375_gene_list[:-1]), ax[4])
-    Network(pd.DataFrame(w_ha1e, index=A375_gene_list[:-1], columns=A375_gene_list[:-1]), ax[6])
-    Network(pd.DataFrame(w_ht29, index=A375_gene_list[:-1], columns=A375_gene_list[:-1]), ax[8])
+    Network(pd.DataFrame(w, index=list_gene[:-1], columns=list_gene[:-1]), ax[0])
+    ax[0].axis('off')
+    ax[0].set_title("gene-gene network " + cellLine)
 
-    for i in [0, 2, 4, 6, 8]:
-        ax[i].axis('off')
-    ax[0].set_title("gene-gene network Melanoma")
-    ax[2].set_title("gene-gene network A375")
-    ax[4].set_title("gene-gene network A549")
-    ax[6].set_title("gene-gene network HA1E")
-    ax[8].set_title("gene-gene network HT29")
-
-    # plot over time
+    # plot ode solution over time
     ts = np.arange(0.0, 48, 4)
-    # melanoma
-    sol = solver(w_mel, eta_mel[0], ts)
-    # A375
-    sol375 = solver(w_375, eta_375[0], ts)
-    # A549
-    sol549 = solver(w_549, eta_549[0], ts)
-    # HA1E
-    solha1e = solver(w_ha1e, eta_549[0], ts)
-    # HT29
-    solht29 = solver(w_ht29, eta_ht29[0], ts)
+    sol = solver(w, eta[0], ts)
 
     ax[1].plot(ts, sol)
-    ax[1].legend(melanoma_gene_list)
-    ax[1].set_ylabel("expr. level Melanoma")
+    ax[1].legend(list_gene)
+    ax[1].set_ylabel("expr. level " + cellLine)
+    ax[1].set_ylim((0.0, 2750))
+    ax[1].set_xlabel("time")
 
-    ax[3].plot(ts, sol375)
-    ax[3].legend(A375_gene_list)
-    ax[3].set_ylabel("expr. level A375")
 
-    ax[5].plot(ts, sol549)
-    ax[5].legend(A375_gene_list)
-    ax[5].set_ylabel("expr. level A549")
+def makeFigure():
 
-    ax[7].plot(ts, solha1e)
-    ax[7].legend(A375_gene_list)
-    ax[7].set_ylabel("expr. level HA1E")
+    ax, f = getSetup((10, 10), (4, 4))
+    melanoma_gene_list = ['FOSL1', 'FOXC2', 'JUN', 'JUNB', 'neg']
+    gene_list1 = ['FOS', 'FOSL1', 'FOXJ3', 'FOXO3', 'FOXO4', 'JUN', 'control']
+    gene_list2 = ['NFKB2', 'NFKBIA', 'NFKBIB', 'NFKBIE', 'STAT1', 'STAT3', 'STAT5B', 'control']
 
-    ax[9].plot(ts, solht29)
-    ax[9].legend(A375_gene_list)
-    ax[9].set_ylabel("expr. level HT29")
+    network_odesol(ax[0:2], 'A375', gene_list1)
+    network_odesol(ax[2:4], 'A375', gene_list2)
 
-    for i in [1, 3, 5, 7, 9]:
-        ax[i].set_ylim((0.0, 2750))
-        ax[i].set_xlabel("time")
+    network_odesol(ax[4:6], 'A549', gene_list1)
+    network_odesol(ax[6:8], 'A549', gene_list2)
+
+    network_odesol(ax[8:10], 'HA1E', gene_list1)
+    network_odesol(ax[10:12], 'HA1E', gene_list2)
+
+    network_odesol(ax[12:14], 'HT29', gene_list1)
+    network_odesol(ax[14:16], 'HT29', gene_list2)
 
     return f
